820527142b793fbdf655aa63add903874d460b9b
Wout Neirynck <wout@monkey-projects.be>
2023-05-23 16:16:15 +0200

    Enabling reagent

--- src/main/monkey/blog/core.cljs
+++ src/main/monkey/blog/core.cljs
@@ -1,5 +1,28 @@
-(ns monkey.blog.core)
+(ns monkey.blog.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]))
 
-(defn init []
-  ;; TODO
-  "still todo")
+(defn header []
+  [:div.header
+   [:div.site_title
+    [:a {:href ""} "fighting the food fight"]]
+   "uh, " [:i "good"] " fight, I mean good fight."])
+
+(defn footer []
+  [:div.footer
+   [:p "Time for a change, " [:a {:href "https://www.mozilla.org/firefox"} "try firefox"] "."]
+   [:p "Mail me at " [:a {:href "mailto:wout@neirynck.com"} "wout@neirynck.com"] "."]])
+
+(defn main []
+  [:div.main
+   [header]
+   [:p "Content goes here"]
+   [footer]])
+
+(defn ^:dev/after-load reload! []
+  (let [root (js/document.getElementById "app")]
+    (rdom/unmount-component-at-node root)
+    (rdom/render [main] root)))
+
+(defn ^:export init []
+  (reload!))
--- src/test/monkey/blog/test/core_test.cljs
+++ src/test/monkey/blog/test/core_test.cljs
@@ -2,6 +2,14 @@
   (:require [cljs.test :refer-macros [deftest is testing]]
             [monkey.blog.core :as sut]))
 
-(deftest init
-  (testing "exists"
-    (is (some? (sut/init)))))
+(deftest header
+  (testing "returns vector"
+    (is (vector? (sut/header)))))
+
+(deftest footer
+  (testing "returns vector"
+    (is (vector? (sut/footer)))))
+
+(deftest main
+  (testing "returns vector"
+    (is (vector? (sut/main)))))
